ecde87ef79585e729ddb5089ae9b19c39b26c609
Xinwei Zhu <easonzhu@byu.edu>
2026-05-22 06:17:30 -0600

    server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,16 @@
 package server;
 
 import io.javalin.*;
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryClearDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import dataaccess.UserDAO;
+import handler.ClearHandler;
+import io.javalin.Javalin;
+import service.ClearService;
 
 public class Server {
 
@@ -9,8 +19,15 @@ public class Server {
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        // Register your endpoints and exception handlers here.
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
 
+        MemoryClearDAO clearDAO = new MemoryClearDAO(userDAO, authDAO, gameDAO);
+        ClearService clearService = new ClearService(clearDAO);
+        ClearHandler clearHandler = new ClearHandler(clearService);
+
+        javalin.delete("/db", ctx -> clearHandler.clear(ctx));
     }
 
     public int run(int desiredPort) {
