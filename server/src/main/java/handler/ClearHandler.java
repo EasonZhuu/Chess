34424bd5c3dd863dcef8d76fb3f26e084b367666
Xinwei Zhu <easonzhu@byu.edu>
2026-05-22 06:17:13 -0600

    ClearHandler

---
+++ server/src/main/java/handler/ClearHandler.java
@@ -0,0 +1,27 @@
+package handler;
+
+import java.util.Map;
+
+import io.javalin.http.Context;
+import server.ErrorResponse;
+import service.ClearService;
+import service.ServiceException;
+
+public class ClearHandler {
+    private final ClearService clearService;
+
+    public ClearHandler(ClearService clearService){
+        this.clearService = clearService;
+    }
+
+    public void clear(Context ctx){
+        try{
+            clearService.clear();
+            ctx.status(200);
+            ctx.json(Map.of());
+        } catch (ServiceException ex){
+            ctx.status(ex.statusCode());
+            ctx.json(new ErrorResponse(ex.getMessage()));
+        }
+    }
+}
