07f4bf468d1602e261cc2e69dc37679988dd417f
Xinwei Zhu <easonzhu@byu.edu>
2026-06-20 17:54:21 -0600

    leave resign

--- server/src/main/java/websocket/WsRequestHandler.java
+++ server/src/main/java/websocket/WsRequestHandler.java
@@ -1,5 +1,9 @@
 package websocket;
 
+import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPiece;
+import chess.InvalidMoveException;
 import com.google.gson.Gson;
 import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
@@ -13,10 +17,12 @@ import io.javalin.websocket.WsMessageHandler;
 import model.AuthData;
 import model.GameData;
 import org.eclipse.jetty.websocket.api.Session;
+import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
 import websocket.messages.ErrorMessage;
 import websocket.messages.LoadGameMessage;
 import websocket.messages.NotificationMessage;
+
 
 import java.io.IOException;
 
@@ -49,7 +55,10 @@ public class WsRequestHandler implements WsConnectHandler, WsMessageHandler, WsC
 
             switch (command.getCommandType()) {
                 case CONNECT -> connect(command, ctx.session);
-                case MAKE_MOVE -> makeMove(command, ctx.session);
+                case MAKE_MOVE -> {
+                    MakeMoveCommand moveCommand = gson.fromJson(ctx.message(), MakeMoveCommand.class);
+                    makeMove(moveCommand, ctx.session);
+                }
                 case LEAVE -> leave(command, ctx.session);
                 case RESIGN -> resign(command, ctx.session);
             }
@@ -114,16 +123,257 @@ public class WsRequestHandler implements WsConnectHandler, WsMessageHandler, WsC
         connections.broadcastExcept(command.getGameID(), username, new NotificationMessage(notification));
     }
 
-    private void makeMove(UserGameCommand command, Session session) throws IOException {
-        sendError(session, "Error: MAKE_MOVE is not implemented yet");
+    private void makeMove(MakeMoveCommand command, Session session) throws IOException {
+        if (command.getGameID() == null) {
+            sendError(session, "Error: missing game ID");
+            return;
+        }
+
+        ChessMove move = command.getMove();
+        if (move == null) {
+            sendError(session, "Error: missing move");
+            return;
+        }
+
+        AuthData authData;
+        GameData gameData;
+
+        try {
+            authData = authDAO.getAuth(command.getAuthToken());
+            gameData = gameDAO.getGame(command.getGameID());
+        } catch (DataAccessException ex) {
+            sendError(session, "Error: unable to make move");
+            return;
+        }
+
+        if (authData == null) {
+            sendError(session, "Error: invalid auth token");
+            return;
+        }
+
+        if (gameData == null) {
+            sendError(session, "Error: invalid game ID");
+            return;
+        }
+
+        ChessGame game = gameData.game();
+
+        if (game.isGameOver()) {
+            sendError(session, "Error: game is over");
+            return;
+        }
+
+        String username = authData.username();
+        ChessGame.TeamColor playerColor = getPlayerColor(username, gameData);
+
+        if (playerColor == null) {
+            sendError(session, "Error: observers cannot make moves");
+            return;
+        }
+
+        ChessPiece piece = game.getBoard().getPiece(move.getStartPosition());
+
+        if (piece == null) {
+            sendError(session, "Error: no piece at start position");
+            return;
+        }
+
+        if (piece.getTeamColor() != playerColor) {
+            sendError(session, "Error: cannot move opponent piece");
+            return;
+        }
+
+        if (game.getTeamTurn() != playerColor) {
+            sendError(session, "Error: not your turn");
+            return;
+        }
+
+        try {
+            game.makeMove(move);
+        } catch (InvalidMoveException ex) {
+            sendError(session, "Error: invalid move");
+            return;
+        }
+
+        ChessGame.TeamColor nextPlayer = game.getTeamTurn();
+        String checkedUsername = getUsernameForColor(nextPlayer, gameData);
+        String extraNotification = null;
+
+        if (game.isInCheckmate(nextPlayer)) {
+            game.setGameOver(true);
+            extraNotification = checkedUsername + " is in checkmate";
+        } else if (game.isInStalemate(nextPlayer)) {
+            game.setGameOver(true);
+            extraNotification = checkedUsername + " is in stalemate";
+        } else if (game.isInCheck(nextPlayer))
+        {
+            extraNotification = checkedUsername + " is in check";
+        }
+
+        try {
+            gameDAO.updateGame(gameData);
+        } catch (DataAccessException ex) {
+            sendError(session, "Error: unable to save move");
+            return;
+        }
+
+        connections.broadcast(command.getGameID(), new LoadGameMessage(gameData));
+
+        String moveMessage = username + " moved " + describeMove(move);
+        connections.broadcastExcept(command.getGameID(), username, new NotificationMessage(moveMessage));
+
+        if (extraNotification != null) {
+            connections.broadcast(command.getGameID(), new NotificationMessage(extraNotification));
+         }
+    }
+
+    private ChessGame.TeamColor getPlayerColor(String username, GameData gameData){
+        if (username.equals(gameData.whiteUsername())) {
+            return ChessGame.TeamColor.WHITE;
+        }
+
+        if (username.equals(gameData.blackUsername())) {
+            return ChessGame.TeamColor.BLACK;
+        }
+
+        return null;
+    }
+
+    private String getUsernameForColor(ChessGame.TeamColor color, GameData gameData){
+        if (color == ChessGame.TeamColor.WHITE) {
+            return gameData.whiteUsername();
+        }
+        return  gameData.blackUsername();
+    }
+
+    private String describeMove(ChessMove move) {
+        return "(" +
+                move.getStartPosition().getRow() +
+                "," +
+                move.getStartPosition().getColumn() +
+                ") to (" +
+                move.getEndPosition().getRow() +
+                "," +
+                move.getEndPosition().getColumn() +
+                ")";
     }
 
     private void leave(UserGameCommand command, Session session) throws IOException {
-        sendError(session, "Error: LEAVE is not implemented yet");
+        if (command.getGameID() == null) {
+            sendError(session, "Error: missing game ID");
+            return;
+        }
+
+        AuthData authData;
+        GameData gameData;
+
+        try {
+            authData = authDAO.getAuth(command.getAuthToken());
+            gameData = gameDAO.getGame(command.getGameID());
+        } catch (DataAccessException ex) {
+            sendError(session, "Error: unable to leave game");
+            return;
+        }
+
+        if (authData == null) {
+            sendError(session, "Error: invalid auth token");
+            return;
+        }
+
+        if (gameData == null) {
+            sendError(session, "Error: invalid game ID");
+            return;
+        }
+
+        String username = authData.username();
+        GameData updatedGameData = gameData;
+
+        if (username.equals(gameData.whiteUsername())) {
+            updatedGameData = new GameData(
+                    gameData.gameID(),
+                    null,
+                    gameData.blackUsername(),
+                    gameData.gameName(),
+                    gameData.game()
+            );
+        } else if (username.equals(gameData.blackUsername())) {
+            updatedGameData = new GameData(
+                    gameData.gameID(),
+                    gameData.whiteUsername(),
+                    null,
+                    gameData.gameName(),
+                    gameData.game()
+            );
+        }
+
+        if (updatedGameData != gameData) {
+            try {
+                gameDAO.updateGame(updatedGameData);
+            } catch (DataAccessException ex) {
+                sendError(session, "Error: unable to save leave");
+                return;
+            }
+        }
+
+        connections.remove(command.getGameID(), username);
+        connections.broadcast(command.getGameID(), new NotificationMessage(username + " left the game"));
+
+
     }
 
     private void resign(UserGameCommand command, Session session) throws IOException {
-        sendError(session, "Error: RESIGN is not implemented yet");
+        if (command.getGameID() == null) {
+            sendError(session, "Error: missing game ID");
+            return;
+        }
+
+
+        AuthData authData;
+        GameData gameData;
+
+        try {
+            authData = authDAO.getAuth(command.getAuthToken());
+            gameData = gameDAO.getGame(command.getGameID());
+        } catch (DataAccessException ex) {
+            sendError(session, "Error: unable to resign");
+            return;
+        }
+
+        if (authData == null) {
+            sendError(session, "Error: invalid auth token");
+            return;
+        }
+
+        if (gameData == null) {
+            sendError(session, "Error: invalid game ID");
+            return;
+        }
+
+        ChessGame game = gameData.game();
+
+        if (game.isGameOver()) {
+            sendError(session, "Error: game is over");
+            return;
+        }
+
+        String username = authData.username();
+        ChessGame.TeamColor playerColor = getPlayerColor(username, gameData);
+
+        if (playerColor == null) {
+            sendError(session, "Error: observers cannot resign");
+            return;
+        }
+
+        game.setGameOver(true);
+
+        try {
+            gameDAO.updateGame(gameData);
+        } catch (DataAccessException ex){
+            sendError(session, "Error: unable to save resign");
+            return;
+        }
+
+        connections.broadcast(command.getGameID(), new NotificationMessage(username + " resigned"));
     }
 
     private void sendError(Session session, String errorMessage) {
